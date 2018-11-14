d7e560cc2a8e0d7fda736d28a197cde0d4c725cf
unknown <Jakub.Borsuk@student.tdsb.on.ca>
2018-11-14 13:40:55 -0500

    started on parttimestaff class

--- src/payrollrunner/PartTimeStaff.java
+++ src/payrollrunner/PartTimeStaff.java
@@ -9,6 +9,34 @@ package payrollrunner;
  *
  * @author 342720539
  */
-public class PartTimeStaff {
+public class PartTimeStaff extends Employee {
+    private float numHoursAssigned;
+    private double hourlyRate;
+    private float sickDaysTaken = 0;
     
+    public PartTimeStaff(float numHoursAssigned, double hourlyRate, String employeeNum, String first, String last){
+        super(employeeNum, first, last);
+        this.numHoursAssigned = numHoursAssigned;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double pay() {
+        return hourlyRate * (numHoursAssigned - (sickDaysTaken * 8.0));
+        }
+
+    @Override
+    public void deductSickDay(float f) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resetSickDay() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void printPayStub() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- src/payrollrunner/PayrollRunner.java
+++ src/payrollrunner/PayrollRunner.java
@@ -15,8 +15,7 @@ public class PayrollRunner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FullTimeStaff c = new FullTimeStaff(1200,"123123","Hello","w");
-        System.out.println(c.toString());
+
     }
     
 }
